cc2e1653aed95343b2855807279e9dcfa9b5b20e
lisas <lisas@DESKTOP-DBLBE9U>
2020-05-26 20:10:31 +0200

    t6

--- EDA/src/librerias/estructurasDeDatos/grafos/Grafo.java
+++ EDA/src/librerias/estructurasDeDatos/grafos/Grafo.java
@@ -2,73 +2,135 @@ package librerias.estructurasDeDatos.grafos;
 
 import librerias.estructurasDeDatos.modelos.*;
 
-/** Clase abstracta Grafo: 
- *  Base de la jerarquia Grafo, que define el comportamiento de un grafo. 
- *  No es una interfaz porque incluye el codigo de aquellas operaciones de un grafo 
- *  que son independientes tanto de su tipo como de su implementacion.
+/**
+ * Clase abstracta Grafo: Base de la jerarquia Grafo, que define el
+ * comportamiento de un grafo. No es una interfaz porque incluye el codigo de
+ * aquellas operaciones de un grafo que son independientes tanto de su tipo como
+ * de su implementacion.
  */
 
 public abstract class Grafo {
-        
-    /** Devuelve el numero de vertices de un grafo.
-     *  @return int, numero de vertices de un grafo
-     */
-    public abstract int numVertices();
-    
-    /** Devuelve el numero de aristas de un grafo.
-     *  @return int, numero de aristas de un grafo
-     */
-    public abstract int numAristas();
-    
-    /** Comprueba si la arista (i,j) esta en un grafo.
-     *  @param i, Vertice origen
-     *  @param j, Vertice destino
-     *  @return boolean, true si (i,j) esta y false en caso contrario
-     */
-    public abstract boolean existeArista(int i, int j);
-    
-    /** Devuelve el peso de la arista (i,j) de un grafo, 
-     *  0 si dicha arista no esta en el grafo.
-     *  @return double, Peso de la arista (i,j), 0 si no existe.
-     */
-    public abstract double pesoArista(int i, int j);
-    
-    /** Si no esta, inserta la arista (i, j) en un grafo no Ponderado.
-     *  @param i, Vertice origen
-     *  @param j, Vertice destino
-     */
-    public abstract void insertarArista(int i, int j);
-    
-    /** Si no esta, inserta la arista (i, j) de peso p en un grafo Ponderado.
-     *  @param i, Vertice origen
-     *  @param j, Vertice destino
-     *  @param p, Peso de la arista (i,j)
-     */
-    public abstract void insertarArista(int i, int j, double p);
 
-    /** Devuelve una Lista Con PI que contiene los adyacentes al vertice i de un grafo.
-     *  @param i, Vertice del que se obtienen los adyacentes
-     *  @return ListaConPI, con los vertices adyacentes a i
-     */
-    public abstract ListaConPI<Adyacente> adyacentesDe(int i);
-       
-    /** Devuelve un String con cada uno de los vertices de un grafo y sus adyacentes, 
-     *  en orden de insercion 
-     *  @return String, que representa a un grafo
-     */        
-    public String toString() {
-        String res = "" ;  
-        for (int i = 0 ; i < numVertices(); i++) {
-            res += "Vertice: " + i;
-            ListaConPI<Adyacente> l = adyacentesDe(i);
-            res += (l.esVacia()) ? " sin adyacentes " :  " con adyacentes: ";
-            for (l.inicio(); !l.esFin(); l.siguiente()) {
-                res +=  l.recuperar() + " ";  
-            }
-            res += "\n";  
-        }
-        return res;      
-    }  
-     
-    /** Se iran anyadiendo metodos y atributos "auxiliares" conforme avance el tema. */
+	/**
+	 * Devuelve el numero de vertices de un grafo.
+	 * 
+	 * @return int, numero de vertices de un grafo
+	 */
+	public abstract int numVertices();
+
+	/**
+	 * Devuelve el numero de aristas de un grafo.
+	 * 
+	 * @return int, numero de aristas de un grafo
+	 */
+	public abstract int numAristas();
+
+	/**
+	 * Comprueba si la arista (i,j) esta en un grafo.
+	 * 
+	 * @param i, Vertice origen
+	 * @param j, Vertice destino
+	 * @return boolean, true si (i,j) esta y false en caso contrario
+	 */
+	public abstract boolean existeArista(int i, int j);
+
+	/**
+	 * Devuelve el peso de la arista (i,j) de un grafo, 0 si dicha arista no esta en
+	 * el grafo.
+	 * 
+	 * @return double, Peso de la arista (i,j), 0 si no existe.
+	 */
+	public abstract double pesoArista(int i, int j);
+
+	/**
+	 * Si no esta, inserta la arista (i, j) en un grafo no Ponderado.
+	 * 
+	 * @param i, Vertice origen
+	 * @param j, Vertice destino
+	 */
+	public abstract void insertarArista(int i, int j);
+
+	/**
+	 * Si no esta, inserta la arista (i, j) de peso p en un grafo Ponderado.
+	 * 
+	 * @param i, Vertice origen
+	 * @param j, Vertice destino
+	 * @param p, Peso de la arista (i,j)
+	 */
+	public abstract void insertarArista(int i, int j, double p);
+
+	/**
+	 * Devuelve una Lista Con PI que contiene los adyacentes al vertice i de un
+	 * grafo.
+	 * 
+	 * @param i, Vertice del que se obtienen los adyacentes
+	 * @return ListaConPI, con los vertices adyacentes a i
+	 */
+	public abstract ListaConPI<Adyacente> adyacentesDe(int i);
+
+	/**
+	 * Devuelve un String con cada uno de los vertices de un grafo y sus adyacentes,
+	 * en orden de insercion
+	 * 
+	 * @return String, que representa a un grafo
+	 */
+	public String toString() {
+		String res = "";
+		for (int i = 0; i < numVertices(); i++) {
+			res += "Vertice: " + i;
+			ListaConPI<Adyacente> l = adyacentesDe(i);
+			res += (l.esVacia()) ? " sin adyacentes " : " con adyacentes: ";
+			for (l.inicio(); !l.esFin(); l.siguiente()) {
+				res += l.recuperar() + " ";
+			}
+			res += "\n";
+		}
+		return res;
+	}
+
+	// atributos "auxiliares"
+	protected int[] visitados;
+	protected int ordenVisita;
+
+	// metodo toArrayDFS de un vertice
+	protected void toArrayDFS(int v, int[] res) {
+		res[ordenVisita] = v;
+		ordenVisita++;
+		visitados[v] = 1;
+		ListaConPI<Adyacente> l = adyacentesDe(v);
+		for (l.inicio(); !l.esFin(); l.siguiente()) {
+			int w = l.recuperar().getDestino();
+			if (visitados[w] == 0) {
+				toArrayDFS(w, res);
+			}
+
+		}
+	}
+	//metodo toArrayDFS del grafo
+	public int[] toArrayDFS() {
+		int[]res =new int[numVertices()];
+		ordenVisita=0;
+		visitados =new int[numVertices()];
+		for(int v =0; v<numVertices(); v++) {
+			if(visitados[v] == 0) {toArrayDFS(v, res);}
+			
+		}
+		return res;
+	}
+	
+	/**
+	 * Se iran anyadiendo metodos y atributos "auxiliares" conforme avance el tema.
+	 */
+
+	public static void main(String[] args) {
+		GrafoDirigido g = new GrafoDirigido(4);
+
+		g.insertarArista(0, 1);
+		g.insertarArista(2, 0);
+		g.insertarArista(2, 3);
+		g.insertarArista(2, 1);
+
+		System.out.println(g.gradoAlt());
+		System.out.println(g.getFuenteU());
+	}
 }
--- EDA/src/librerias/estructurasDeDatos/grafos/GrafoDirigido.java
+++ EDA/src/librerias/estructurasDeDatos/grafos/GrafoDirigido.java
@@ -25,7 +25,105 @@ public class GrafoDirigido extends Grafo {
             elArray[i] = new LEGListaConPI<Adyacente>();
         }
     }
+    /**grado salida del grafo*/
+    public int gradoSalida() {
+    	//crear e inicializar el array de contadores
+    	int[] cont = new int[numV];
+    	for(int i = 0; i<numV; i++) {
+    		//actualizar el contador de grados con la talla de cada elemento del array 
+    		ListaConPI<Adyacente> v = elArray[i];
+    		cont[i] = v.talla();
+    	}
+    	return maximo(cont);
+    }
+    /**grado salida de un vertice*/
+    public int gradoSalida(int i) {
+    	return elArray[i].talla();
+    }
+    /**grado entrada del grafo*/
+    public int gradoEntrada() {
+    	int[] cont  = new int[numV];//crear e inicializar el array de contadores
+    	for(int i = 0; i<numV; i++) {
+    		//actualizaar el contador del grado de Entrada de cada vertice
+    		//de la lista adyacenteDe(i)
+    		ListaConPI<Adyacente> a = elArray[i];
+    		for(a.inicio(); !a.esFin(); a.siguiente()) {
+    		cont[a.recuperar().getDestino()]++;
+    		}
+    	}
+    	return maximo(cont);
+    }
     
+    /**grado entrada de un vertice
+     */
+    public int gradoEntrada(int i) {
+    	int grado = 0;
+    	for(int j = 0; j < numV; j++) {
+    		if(existeArista(j, i)) grado ++;
+    	}
+    	return grado; //el grado es el valor del grado de entrada de i
+    }
+    
+    /**metodo alternativo para devolver el grado del grafo*/
+    public int gradoAlt() {
+    	int[] gradosEntr = new int[numV];
+    	int[] gradosSal = new int[numV];
+    	for(int i = 0; i<numV; i++) {
+    		gradosEntr[i] = gradoEntrada(i);
+    		gradosSal[i] = gradoSalida(i);
+    	}
+    	int[] contGrado = new int[numV];
+    	for(int i = 0; i<numV; i++) {
+    		contGrado[i] = gradosEntr[i] + gradosSal[i];
+    	}
+    	 return maximo(contGrado);                          
+    }
+    /**Devuelve el grado del grafo
+     * 
+     * @return
+     */
+    public int grado() {
+    	//Paso 1: almacenar en un array los grados de todos los vertices
+    	//Paso 2: sacar el maximo grado de los vertices
+    	
+    	int[] v = getArrayGrados();
+    	return maximo(v);
+    	
+    	
+    }
+    
+    protected int[] getArrayGrados() {
+    	int[] gradosV = new int[numV];
+    	for(int i =0; i<numV; i++) {
+    		ListaConPI<Adyacente> l = elArray[i];
+    		//Actualizar grado vérticce i con su grado de salida
+    		gradosV[i] += l.talla();
+    		//Actualizar grado vertices *adyacentes* a i con "parte" de su grado de entrada
+    		for(l.inicio();!l.esFin();l.siguiente())
+    			gradosV[l.recuperar().getDestino()]++;
+    	}
+    	return gradosV;
+    }
+    
+    protected int maximo(int[] v) {
+    	int max = v[0];
+    	for(int i =1; i < v.length; i++) {
+    		if(v[i]>max) {max = v[i];}
+    	}
+    	return max;
+    }
+    
+    public double pesoMax() {
+    	double res = -1;
+    	for(int i = 0; i<numV; i++) {
+    		ListaConPI<Adyacente> lista = elArray[i];
+    		for(lista.inicio();!lista.esFin(); lista.siguiente()) {
+    			double a = lista.recuperar().getPeso();
+    			if(a > res) res = a;
+    		}
+    	}
+    	return res;
+    }
     /** Devuelve el numero de vertices de un grafo. 
      *  @return int, Numero de vertices de un grafo
      */
@@ -92,5 +190,50 @@ public class GrafoDirigido extends Grafo {
      */
     public ListaConPI<Adyacente> adyacentesDe(int i) { 
         return elArray[i]; 
+    }
+    
+    public int gradoV(int i) {
+    	return gradoEntrada(i) + gradoSalida(i);
+    }
+    
+    public boolean esRegular() {
+    	int grado = gradoEntrada(0) + gradoSalida(0);
+    	
+    	for(int i = 1; i<numV; i++) {
+    		int g = gradoEntrada(i) + gradoSalida(i);
+    		if(grado != g) return false;
+    	}
+    	return true;
+    }
+    
+    public int getVerticeReceptivo() {
+    	
+    	for(int i = 0; i<numV; i++) {
+    		if(gradoEntrada(i) == numV - 1) return i;
+    	}
+    	return -1;
+    }
+    
+    public int getSumideroU() {
+    	for(int i = 0; i<numV; i++) {
+    		if(gradoEntrada(i) == numV-1 && gradoSalida(i) == 0) return i;
+    	}
+    	return -1;
+    }
+    
+    public boolean esSumidero(int i) {
+    	if(gradoEntrada(i) > 0 && gradoSalida(i) == 0) return true;
+    	else return false;
+    }
+    
+    public int getFuenteU() {
+    	for(int i = 0; i<numV; i++) {
+    		if(gradoEntrada(i) == 0 && gradoSalida(i) == numV - 1)return i;
+    	}
+    	return -1;
+    }
+    
+    public boolean esCompleto() {
+    	return numA == numV * (numV - 1);
     }
 }
